166d536bdf5b715ca87957f9d53a9dc4b327ed83
ChristianAyala <cayala@smu.edu>
2019-09-08 23:48:35 +0000

    Updates

---
+++ docs/navtreedata.js
@@ -0,0 +1,20 @@
+var NAVTREE =
+[
+  [ "SMU KNW 2300 Arduino Library", "index.html", [
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"_k_n_w_robot_8h_source.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
