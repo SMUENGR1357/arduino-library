9a49ae7c06b8d7d5a2579dbccfd73998c0a6df8a
ChristianAyala <cayala@smu.edu>
2019-09-08 23:57:31 +0000

    Updates

--- navtreedata.js
+++ navtreedata.js
@@ -1,25 +1,3 @@
-/*
-@licstart  The following is the entire license notice for the
-JavaScript code in this file.
-
-Copyright (C) 1997-2019 by Dimitri van Heesch
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of version 2 of the GNU General Public License as published by
-the Free Software Foundation
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-@licend  The above is the entire license notice
-for the JavaScript code in this file
-*/
 var NAVTREE =
 [
   [ "SMU KNW 2300 Arduino Library", "index.html", [
@@ -27,8 +5,8 @@ var NAVTREE =
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ]
     ] ],
-    [ "Files", "files.html", [
-      [ "File List", "files.html", "files_dup" ]
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ]
     ] ]
   ] ]
 ];
--- search/classes_0.js
+++ search/classes_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['knwrobot_1',['KNWRobot',['../class_k_n_w_robot.html',1,'']]]
+  ['knwrobot',['KNWRobot',['../class_k_n_w_robot.html',1,'']]]
 ];
