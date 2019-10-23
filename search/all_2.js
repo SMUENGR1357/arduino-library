1e7d1e7d8a4edab08f8efb3ca93cebef1c805ffc
ChristianAyala <cayala@smu.edu>
2019-10-23 20:34:06 +0000

    Updates

--- search/all_2.js
+++ search/all_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['knwrobot',['KNWRobot',['../class_k_n_w_robot.html',1,'KNWRobot'],['../class_k_n_w_robot.html#a646b4429008350a0db1b4a544703c5cb',1,'KNWRobot::KNWRobot()']]]
+  ['knwrobot',['KNWRobot',['../class_k_n_w_robot.html',1,'KNWRobot'],['../class_k_n_w_robot.html#a55703f81f04348fcff64f4946fb64250',1,'KNWRobot::KNWRobot()']]]
 ];
--- search/all_6.js
+++ search/all_6.js
@@ -4,6 +4,7 @@ var searchData=
   ['setupbump',['setupBump',['../class_k_n_w_robot.html#a6d3863838f5fae8481f70c71cc6fad04',1,'KNWRobot']]],
   ['setupincline',['setupIncline',['../class_k_n_w_robot.html#a44560025bb80b5fbebc42713582d927d',1,'KNWRobot']]],
   ['setupir',['setupIR',['../class_k_n_w_robot.html#a1d8eaa7d3d286d00f8a48f5bb6049e14',1,'KNWRobot']]],
+  ['setupkeypad',['setupKeypad',['../class_k_n_w_robot.html#a9eea5bcedf6f7cd2685b5e1811feec5a',1,'KNWRobot']]],
   ['setupmotor',['setupMotor',['../class_k_n_w_robot.html#a79c83d9b2f0d080d7725964d2bcfc8dc',1,'KNWRobot']]],
   ['setupping',['setupPing',['../class_k_n_w_robot.html#ab9d1d1ded6ca8ce7b445ae26de76dff0',1,'KNWRobot']]],
   ['setupservo',['setupServo',['../class_k_n_w_robot.html#a94753ae378d8167ed01912d315d45da5',1,'KNWRobot']]],
