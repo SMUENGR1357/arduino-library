8fa7a14e3b287b6177cd18b53d7b0aa23c055381
ChristianAyala <chr.ayala@gmail.com>
2020-09-12 14:22:51 -0500

    Added more comments to sample code

--- samples/sample_data_logger.ino
+++ samples/sample_data_logger.ino
@@ -32,46 +32,57 @@
 
 Sleep sleep;
 unsigned char mode;
-unsigned int count = 0;
 unsigned int seconds = 0;
-int analogPinReading;
 
-void setup() {
+void setupDataLoggingMode() {
+  resetSampleCount();
+}
+
+void showContentsOfEEPROM() {
   unsigned int i = 0;
+  unsigned int count = 0;
   unsigned int address;
-  
-  sleep.pwrDownMode(); //set sleep mode
-  seconds = 0;
-  pinMode(2,INPUT_PULLUP);
-  mode = digitalRead(2);
-  if (mode == LOW) {  // data logging mode
-    resetSampleCount();
-  }
-  else {  // data retrieval mode
-    Serial.begin(9600);  // open serial port
-    delay(1000);
-    count = getCountOfDataSamples();
-    while (i < count) {  // loop over all logged data samples
-      address = (BYTES_PER_INTEGER * i) + BYTES_FOR_SAMPLE_COUNTER;  // calculate EEPROM address of next sample
-      analogPinReading = getIntValueAtAddress(address);  // retrieve ADC code of next sample
-      Serial.print(i);  // report sample index and ADC value up serial port
-      Serial.print(':');
-      Serial.println(analogPinReading);
-      i++;
-    }
+  int analogPinReading;
+
+  Serial.begin(9600);  // open serial port to print to the serial console
+  delay(1000);
+  count = getCountOfDataSamples();
+  while (i < count) {  // loop over all logged data samples
+    address = (BYTES_PER_INTEGER * i) + BYTES_FOR_SAMPLE_COUNTER;  // calculate EEPROM address of next sample
+    analogPinReading = getIntValueAtAddress(address);  // retrieve ADC code of next sample
+    Serial.print(i);  // report sample index and ADC value up serial port
+    Serial.print(':');
+    Serial.println(analogPinReading);
+    i++;
   }
 }
 
+void gatherAndStoreAnalogReading() {
+  int analogPinReading;
+  sleep.sleepDelay(1000);  // delay 1 second in low power mode
+  seconds++;
+  if (seconds >= 60) {  // test if one minute has elapsed since last sample
+    seconds = 0;
+
+    // Get a "sample". This can be replaced with some other function that provides
+    // data that you want to store (e.g. getConductivity())
+    analogPinReading = analogRead(0);
+    appendIntValueToEEPROM(analogPinReading); // Write to EEPROM
+  }
+}
+
+// This function is called ONCE when your arduino first gets power
+void setup() {
+  sleep.pwrDownMode(); //set sleep mode
+  pinMode(2,INPUT_PULLUP); // See if digital pin 2 is set to high
+  mode = digitalRead(2);
+
+  if (mode == LOW) setupDataLoggingMode(); // data logging mode
+  else showContentsOfEEPROM(); // data retrieval mode
+}
+
+// This function is called REPEATEDLY, after setup() is done
 void loop() {
-  if (mode == LOW) {  // data logging mode
-    sleep.sleepDelay(1000);  // delay 1 second in low power mode
-    seconds++;
-    if (seconds >= 60) {  // test if one minute has elapsed since last sample
-      seconds = 0;
-      analogPinReading = analogRead(0);  // sample ADC code
-      appendIntValueToEEPROM(analogPinReading);
-    }
-  }
-  else {  // data retrieval mode
-  }
+  if (mode == LOW) gatherAndStoreAnalogReading(); // data logging mode
+  // else Do Nothing, because we've already retrieved the contents
 }
