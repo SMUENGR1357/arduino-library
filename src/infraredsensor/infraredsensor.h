e837d4816c7bb442aacae2133bcc13721c4e19e3
ChristianAyala <chr.ayala@gmail.com>
2021-09-11 13:00:56 -0500

    Added documentation

--- src/infraredsensor/infraredsensor.h
+++ src/infraredsensor/infraredsensor.h
@@ -16,9 +16,38 @@ unsigned char IRChar, IRCharBitMask, buffer[8];
 bool receiverState = false;
 unsigned long cur_time, ticks;
 
-// ******************************************* //
-// Function to read IR character from sensor.
-// ******************************************* //
+/**
+ * This uses the IR sensor at the given digital pin to scan for beacons that
+ * may be nearby.
+ *
+ * This function uses the IR sensor to scan for IR signals coming from the beacons.
+ * The values that it reads are stored in an internal character buffer, which you
+ * can access using getIR() after running this function.
+ *
+ * <b>Note</b>: the function will return -1 if the pin passed for scanIR() is invalid
+ * 
+ * <b>Note</b>: every time you want to get fresh values from getIR(), you have to
+ * run this function first.
+ *
+ * @param pin The digital pin number that the IR sensor is connected to.
+ * @return int The number of characters that were read in this given scan.
+ *
+ * Example code:
+ *
+ * @code
+ * #include <infraredsensor.h>
+ * 
+ * int IRSensorPin = 20;
+ * int numCharsReadFromIR = scanIR(IRSensorPin);
+ * myRobot->printLCD("Chars read: ");
+ * myRobot->printLCD(charactersReadFromIR);
+ *
+ * // Now print the reading on the next line of the LCD
+ * char* IRCharacters = getIR();
+ * myRobot->moveCursor(0, 1);
+ * myRobot->printLCD(IRCharacters);
+ * @endcode
+ */
 int scanIR(int pin)
 {
     if (pin != -1)
@@ -145,6 +174,33 @@ int scanIR(int pin)
     return -1; // this is for incorrect ID
 }
 
+/**
+ * This provides the characters that were reading from the most recent call to scanIR().
+ *
+ * This function should be used in conjunction with scanIR(). Every time you want a fresh
+ * IR reading, call scanIR() first. Then, call this function, storing the result in a char*
+ * variable. If you don't call scanIR() before calling this, the internal character buffer
+ * will not refresh and this will continuously provide the same value (or an empty value
+ * if you never ran scanIR() in the first place).
+ *
+ * @return char* A character buffer containing the characters read from the most recent
+ * call to scanIR()
+ *
+ * Example code:
+ *
+ * @code
+ * #include <infraredsensor.h>
+ * 
+ * int charactersReadFromIR = scanIR(IRSensorId);
+ * myRobot->printLCD("Chars read: ");
+ * myRobot->printLCD(charactersReadFromIR);
+ *
+ * // Now print the reading on the next line of the LCD
+ * char* IRCharacters = getIR();
+ * myRobot->moveCursor(0, 1);
+ * myRobot->printLCD(IRCharacters);
+ * @endcode
+ */
 unsigned char *getIR()
 {
     return buffer;
